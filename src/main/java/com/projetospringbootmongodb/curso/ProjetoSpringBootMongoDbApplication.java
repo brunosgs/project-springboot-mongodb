cdd226b080ea89db759523bfac1a2c06a430d0bd
Bruno Gomes <brunosoges@gmail.com>
2021-06-16 22:03:12 -0300

    Initial commit

---
+++ src/main/java/com/projetospringbootmongodb/curso/ProjetoSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.projetospringbootmongodb.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetospringbootmongodb/curso/ProjetoSpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospringbootmongodb.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
