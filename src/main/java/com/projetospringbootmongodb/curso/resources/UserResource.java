893e329053e4290ab3a8fc2a5fdcfb47dd52ef2a
Bruno Gomes <brunosoges@gmail.com>
2021-06-21 13:11:53 -0300

    Atualização de usuário com PUT

--- src/main/java/com/projetospringbootmongodb/curso/resources/UserResource.java
+++ src/main/java/com/projetospringbootmongodb/curso/resources/UserResource.java
@@ -48,6 +48,17 @@ public class UserResource {
 		return ResponseEntity.ok().body(listUserDTO);
 	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+		User user = userService.fromDTO(userDTO);
+
+		user.setId(id);
+
+		user = userService.update(user);
+
+		return ResponseEntity.noContent().build();
+	}
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		userService.delete(id);
--- src/main/java/com/projetospringbootmongodb/curso/services/UserService.java
+++ src/main/java/com/projetospringbootmongodb/curso/services/UserService.java
@@ -30,6 +30,13 @@ public class UserService {
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+
+		return userRepository.save(newUser);
+	}
+
 	public void delete(String id) {
 		findById(id);
 		userRepository.deleteById(id);
@@ -37,5 +44,10 @@ public class UserService {
 
 	public User fromDTO(UserDTO userDTO) {
 		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+	}
+
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
 	}
 }
