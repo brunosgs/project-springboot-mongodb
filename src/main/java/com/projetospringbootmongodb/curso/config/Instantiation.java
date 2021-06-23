b6736a55c7752ab6fe54ab709cec300a01d76637
Bruno Gomes <brunosoges@gmail.com>
2021-06-22 21:23:11 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/projetospringbootmongodb/curso/config/Instantiation.java
+++ src/main/java/com/projetospringbootmongodb/curso/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.projetospringbootmongodb.curso.domain.Post;
 import com.projetospringbootmongodb.curso.domain.User;
 import com.projetospringbootmongodb.curso.dto.AuthorDTO;
+import com.projetospringbootmongodb.curso.dto.CommentDTO;
 import com.projetospringbootmongodb.curso.repositories.PostRepository;
 import com.projetospringbootmongodb.curso.repositories.UserRepository;
 
@@ -40,8 +41,15 @@ public class Instantiation implements CommandLineRunner {
 				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+		CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO comment2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().add(comment3);
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
+
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
 	}
--- src/main/java/com/projetospringbootmongodb/curso/domain/Post.java
+++ src/main/java/com/projetospringbootmongodb/curso/domain/Post.java
@@ -1,12 +1,15 @@
 package com.projetospringbootmongodb.curso.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.projetospringbootmongodb.curso.dto.AuthorDTO;
+import com.projetospringbootmongodb.curso.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,7 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 
@@ -70,6 +74,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
